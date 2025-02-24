60f8a1fe026a711babe45ce3f3a3c6ee63b7fbe4
syed-mahroof <syedmahroof2002@gmail.com>
2025-02-24 14:20:18 +0530

    first commit

--- myproject/myapp/urls.py
+++ myproject/myapp/urls.py
@@ -2,5 +2,5 @@ from django.urls import path
 from . import views 
 
 urlpatterns =[
-    path("",views.msg,name="msg")
+    path("",views.home,name='home'),
 ]
--- myproject/myapp/views.py
+++ myproject/myapp/views.py
@@ -2,5 +2,8 @@ from django.shortcuts import render
 from django.http import HttpResponse
 # Create your views here.
 
-def msg(request):
-    return HttpResponse("Hello World")
+# def msg(request):
+#     return HttpResponse("Hello World")
+
+def home(request):
+    return render(request, 'home.html')
